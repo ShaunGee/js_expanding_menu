e17a8aa46abf4ea50cd498b7e89a608f0626c5b7
Shaun Gumbaketi <shaungumbaketi@gmail.com>
2024-04-28 17:12:03 +1000

    breakpoint added at 500px

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function menu_transform(){
+    let screenWidth = window.innerWidth
+
+    if (screenWidth <= 500){
+        //hamburgerMenu();
+    }
+
+
+}
+//hamburgerMenu(){=}
+
+
+menu_transform()
+window.addEventListener('resize',()=>{
+    menu_transform();
+    
+});
+
