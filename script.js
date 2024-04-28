42cacab5f3e5308eb910c8165dc9e3f36f7c9393
Shaun Gumbaketi <shaungumbaketi@gmail.com>
2024-04-28 21:03:39 +1000

    hamburger menu animation and positioning added

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+let normalMenu = document.getElementById('nav_menu');
+let hamburgerMenu = document.getElementById('hamburger_menu_container');
 
+const hamburger_menu_lines = document.querySelectorAll('.hamburger_menu_line');
+let hamburgerMenuStatus =true;
 
 function menu_transform(){
     let screenWidth = window.innerWidth
 
     if (screenWidth <= 500){
-        //hamburgerMenu();
+        createHamburgerMenu();
+    }
+    if (screenWidth>500){
+        largePageMenu()
     }
 
 
 }
-//hamburgerMenu(){=}
+
+function largePageMenu(){
+    normalMenu.style.display = 'flex';
+    hamburgerMenu.style.display='none';
+}
+
+function createHamburgerMenu(){
+
+
+    //make current menu disapear
+    normalMenu.style.display = 'none';
+    hamburgerMenu.style.display='flex';
+
+}
 
 
 menu_transform()
@@ -17,4 +37,58 @@ window.addEventListener('resize',()=>{
     menu_transform();
     
 });
+
+hamburgerMenu.addEventListener('click', (event)=>{
+
+    
+    if(hamburgerMenuStatus){
+        hbMenuOpen();
+    }
+    else{
+        hbMenuClose();
+    }
+
+
+    
+
+
+});
+
+function hbMenuOpen(){
+   
+        //remove middle line
+        hamburger_menu_lines[1].style.display = 'none';
+
+        //rotate lines
+        hamburger_menu_lines[0].style.transform = 'rotate(50deg)'
+        hamburger_menu_lines[2].style.transform = 'rotate(-50deg)'
+    
+        //move origin
+        hamburger_menu_lines[0].style.transformOrigin = '10% 20%';
+        hamburger_menu_lines[2].style.transformOrigin = '50% 90%';
+        hamburgerMenuStatus = false;
+
+        
+    
+}
+
+function hbMenuClose(){
+      
+        //add middle line
+        hamburger_menu_lines[1].style.display = 'block';
+
+        //add lines
+        hamburger_menu_lines[0].style.transform = 'rotate(0deg)'
+        hamburger_menu_lines[2].style.transform = 'rotate(0deg)'
+    
+        //return origin
+        hamburger_menu_lines[0].style.transformOrigin = '0';
+        hamburger_menu_lines[2].style.transformOrigin = '0';
+        hamburgerMenuStatus=true;
+    
+}
+
+
+
+//
 
